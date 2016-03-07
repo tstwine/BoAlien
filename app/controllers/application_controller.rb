963468d57a7e8954a230e9bb335d406d28527e9b
Tina Stith-Twine <tinatwine@gmail.com>
2016-03-06 19:35:17 -0500

    Updated Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  before_action :configure_permitted_paramaters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  	def configure_permitted_paramaters
-	  		devise_paramater_sanitizer.for(:sign_up) << :fullname
-	  		devise_paramater_sanitizer.for(:account_update) << :fullname
+  	def configure_permitted_parameters
+	  		devise_parameter_sanitizer.for(:sign_up) << :fullname
+	  		devise_parameter_sanitizer.for(:account_update) << :fullname
   	end	
 end
